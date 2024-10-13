a4cc96d9a0ee9dce12e5ebf647f5a499fe1ed9c2
lunariusis <lunariusis@gmail.com>
2024-10-14 00:59:29 +0300

    update gw

--- projects/games/gw/gw.service.worker.js
+++ projects/games/gw/gw.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1728856296|30593356';
+const CACHE_VERSION = '1728856715|219559599';
 /** @type {string} */
 const CACHE_PREFIX = 'Garbageworld-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
